273571925119517e1a9a1c0d4bef5b8f59fed7db
Jessica Swenson <jessicamswens@gmail.com>
2023-10-27 17:42:14 -0600

    initial commit

---
+++ src/assets/data.js
@@ -0,0 +1,7 @@
+export const sqlPostgresSqlSequelizeObj = [
+    {
+        questionId: 1,
+        question: 'What is SQL, and how does it relate to databases?',
+        answer: 'SQL stands for Structured Query Language. It is a domain-specific language (specific to relational databases) used for managing and manipulating relational databases. SQL allows you to perform operations like querying, inserting, updating, and deleting data in a structured way.'
+    }
+]
