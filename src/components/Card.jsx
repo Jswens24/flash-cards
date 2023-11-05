678c4ec59ee20eae50ca6a82225554bf3aeb5f7c
Jessica Swenson <jessicamswens@gmail.com>
2023-11-05 14:45:25 -0700

    added redirect

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -20,23 +20,21 @@ const Card = ({ data }) => {
         setFlip(true)
     }
 
-    console.log(flip);
-
 
     return (
-        <>
-            <h4>Click on the card to see the answer</h4>
+        <div className={flip === false ? 'flex flex-col justify-center items-center bg-green-50' : 'flex flex-col justify-center items-center'}>
+            <h4>Click on the card to see the {flip === false ? 'question' : 'answer'}</h4>
             <div className='max-w-sm rounded overflow-hidden shadow-lg px-8 py-6 flex flex-col justify-center items-center text-center'>
                 <div onClick={() => setFlip(!flip)} className={flip === true ? 'font-bold text-xl mb-2' : 'hidden'}>
                     <h2>Question: <br />{question}</h2>
                 </div>
-                <div onClick={() => setFlip(!flip)} className={flip === true ? 'hidden' : 'font-bold text-xl mb-2 text-gray-700 text-base'}>
+                <div onClick={() => setFlip(!flip)} className={flip === true ? 'hidden' : 'font-bold text-xl mb-2 text-gray-700 text-base '}>
                     <h3>Answer: <br />{answer}</h3>
                 </div>
                 {data.length - 1 === idCount ? <Link to='/'><button onClick={() => setFlip(true)} className='bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2'>Back to Home</button></Link> : idCount === 0 ? <button className='bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2' onClick={nextCardHandler}>Next Question</button> : <div> <button className='bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2' onClick={lastCardHandler}>Last Question</button> <button className='bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2' onClick={nextCardHandler}>Next Question</button> </div>
                 }
             </div >
-        </>
+        </div>
     )
 }
 
